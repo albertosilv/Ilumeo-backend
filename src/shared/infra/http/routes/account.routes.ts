0f7768e51f961ef04b8e3db8601dc48c5a546555
Alberto Silva <alberto.silva5456@gmail.com>
2023-03-23 01:49:03 -0300

    feat: route session and add middleware ensure Authenticated

--- src/shared/infra/http/routes/account.routes.ts
+++ src/shared/infra/http/routes/account.routes.ts
@@ -2,9 +2,13 @@ import { Router } from 'express';
 
 import { CreateAccountController } from '~modules/account/use-cases/create-account/create-account-controller';
 import { FindAccountController } from '~modules/account/use-cases/find-account/find-account-controller'
+import { CreateSessionController } from '~modules/account/use-cases/create-session/create-session-controller';
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
+
 const accountRoutes = Router();
 
 const createAccountController = new CreateAccountController();
+const createSessionController = new CreateSessionController();
 const findAccountController = new FindAccountController()
 
 accountRoutes.post(
@@ -12,8 +16,15 @@ accountRoutes.post(
   createAccountController.handle,
 );
 
+
+accountRoutes.post(
+  '/session',
+  createSessionController.handle,
+);
+
 accountRoutes.get(
   '/:id',
+  ensureAuthenticated,
   findAccountController.handle,
 );
 
--- src/shared/infra/http/routes/timeRecord.routes.ts
+++ src/shared/infra/http/routes/timeRecord.routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import { CreateTimeRecordController } from '../../../../modules/time-record/use-cases/create-time-record/create-time-record-controller';
 import { GetAllTimeRecordController } from '../../../../modules/time-record/use-cases/get-All-Time-Record/get-all-time-record-controller';
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 
 const timeRecordRoutes = Router();
 
@@ -10,11 +11,13 @@ const getAllTimeRecordController = new GetAllTimeRecordController()
 
 timeRecordRoutes.post(
   '/register',
+  ensureAuthenticated,
   createTimeRecordController.handle,
 );
 
 timeRecordRoutes.get(
   '/:id',
+  ensureAuthenticated,
   getAllTimeRecordController.handle,
 );
 
